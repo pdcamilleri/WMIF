632c5d71703d9933761adcfc1c69260849ef1776
pdcamilleri <pdc.cse@gmail.com>
2014-08-13 15:45:11 +1000

    Adding 3 columns for the presentation of input data

--- WMIF.php
+++ WMIF.php
@@ -1,7 +1,8 @@
 <?php>
-	//Collect all the form data
-	$ip = $_SERVER["REMOTE_ADDR"];
-   $mid = $_SESSION['mid'];
+  session_start();
+  //Collect all the form data
+  $ip = $_SERVER["REMOTE_ADDR"];
+  $mid = $_SESSION['mid'];
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
    <head>
@@ -9,25 +10,46 @@
       <meta http-equiv="Expires" content="0" />
       <meta http-equiv="Cache-Control" content="no-cache" />
       <meta http-equiv="Pragma" content="no-cache" />
-      <title>Decision-making Game</title>
+
+      <!-- JQuery -->
       <link rel="stylesheet" type="text/css" media="screen" href="css/jquery.css" />
       <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
       <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+
+      <!-- My own stuff -->
       <link rel="stylesheet" type="text/css" href="css/main.css" />
       <script type="text/javascript" src="js/WMIF.js"></script>
-      <script type="text/javascript">
-      	var mid = <?php echo json_encode($mid); ?>;
-      </script>
+
+      <!-- Bootstrap -->
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+      <title>Decision-making Game</title>
    </head>
    <body>
       <div id="container">
-      <?php
-        print_r($mid);
-      ?>
+      <p> MID is
+        <?php
+          print_r($mid);
+        ?>
+      </p>
 
-      <button id="testButton" onclick="testdb()">
-        Test Database Button
-      </button>
+      <!-- Just have all 3 columns present, and change things using JS -->
+      <div id="content-left" class="column col-md-4">
+        <p> left</p>
+      </div>
+
+      <div id="content-center" class="column col-md-4">
+        <p> center</p>
+      </div>
+
+      <div id="content-right" class="column col-md-4">
+        <p> right</p>
+      </div>
+
+      <p>
+        Some text afterwards
+      </p>
 
       </div>
    </body>
