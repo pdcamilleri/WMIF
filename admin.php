db08ff1fb6ae608095b70c79496dba1b828759bb
pdcamilleri <pdc.cse@gmail.com>
2014-09-03 23:51:27 +1000

    #1: adding admin.php page and associated config file

---
+++ admin.php
@@ -0,0 +1,25 @@
+<?php>
+  session_start();
+  //Collect all the form data
+  $ip = $_SERVER["REMOTE_ADDR"];
+  $mid = $_SESSION['mid'];
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="Expires" content="0" />
+    <meta http-equiv="Cache-Control" content="no-cache" />
+    <meta http-equiv="Pragma" content="no-cache" />
+
+    <title>Admin Control Panel</title>
+  </head>
+  <body>
+    <?php   
+      $config = parse_ini_file("config.ini");
+
+      print_r($config);
+    
+    ?>
+  </body>
+</html> 
+
