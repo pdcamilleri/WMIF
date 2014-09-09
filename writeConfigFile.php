744f8aee9204028a8c6e9b7e9c6f35a684105e83
pdcamilleri <pdc.cse@gmail.com>
2014-09-09 23:51:18 +1000

    Small changes to writeConfigFile.php

--- writeConfigFile.php
+++ writeConfigFile.php
@@ -1,28 +1,35 @@
 <?php
+/*
+   This file just changes the values in a config file if the associated
+   key is present in the POST parameters.
+
+*/
 
 define("CONFIG_FILE", "config.ini");
-
 // $config = parse_ini_file(CONFIG_FILE);
 
 $fp = fopen(CONFIG_FILE, "r") or die("Can't open file: " . CONFIG_FILE);
 
-// store updated file contents and do a single write back to the file at the end
+// the approach is going to be to store the updated file contents as we go
+// and do a single write back to the file at the end
 $updatedFileContents = "";
 
 // read file line by line. change value if a matching key exists in POST parameters
 while (($line = fgets($fp)) !== false) {
 
-  $pattern = '/([^;].*) = (.*)/'; // match a key and value, if first char is not a ';'
+  $pattern = '/([^;].*) = (.*)/'; 
+  // match a key and value, if first char is not a ';'/comment
+  // might be a source of BUGS! when dealing with things, eg strings?
 
   if (preg_match($pattern, $line, $matches)) {
     $key = $matches[1];
 
     if (isset($_POST[$key])) {
-      $property = $_POST[$key];
+      $value = $_POST[$key];
 
       // update the value associated with this key
       $updatePattern = '/(.*) = (.*)/';
-      $line = preg_replace($updatePattern, "$1 = $property", $line);
+      $line = preg_replace($updatePattern, "$1 = $value", $line);
     }
   }
 
@@ -32,7 +39,7 @@ while (($line = fgets($fp)) !== false) {
 fclose($fp);
 
 // write all of our changes back to the file
-file_put_contents(CONFIG_FILE, $updatedFileContents);
+file_put_contents(CONFIG_FILE, $updatedFileContents) or die ("Unable to write to file: " . CONFIG_FILE);
 
 return json_encode($updatedFileContents);
 
