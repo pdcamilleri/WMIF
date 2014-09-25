81ab16b42d22e2da4adb655961fdcb33c73f798b
pdcamilleri <pdc.cse@gmail.com>
2014-09-25 21:17:52 +1000

    Fixes #32 and #33: simplified interchange between admin page and config file

--- js/admin.js
+++ js/admin.js
@@ -1,53 +1,22 @@
 // saves the settings specified by the experimenter by writing them to config.ini file
 function saveSettings() {
 
-  // TODO will need to come back and make this more general for when there are multiple problems
-  var filter = $('input:checkbox').map(function() {
+  var checkboxes = $('input:checkbox').map(function() {
     return { 
       name: this.name, 
       value: this.checked ? this.value : false
     };
   });
 
-  // TODO use serialzeArray?
-  filter.push({ 
-    name: "samples", 
-    value: $("#samples").val() 
-  });
+  var allInputs = $("form").serializeArray();
 
-  filter.push({ 
-    name: "productInformation", 
-    value: $("#productInformation").val()
-  });
-
-  filter.push({ 
-    name: "expertise", 
-    value: $("#expertise").val()
-  });
-
-
-  filter.push({ 
-    name: "samples2", 
-    value: $("#samples2").val() 
-  });
-
-  filter.push({ 
-    name: "productInformation2", 
-    value: $("#productInformation2").val()
-  });
-
-  filter.push({ 
-    name: "expertise2", 
-    value: $("#expertise2").val()
-  });
-
-
-
+  var combinedInputs = $.makeArray(checkboxes).concat(allInputs);
+  console.log(combinedInputs);
 
   $.ajax({
     type: "POST",
     url: "writeConfigFile.php",
-    data: filter,
+    data: combinedInputs,
     success: function() {
       d("Settings Saved!");
     },
@@ -67,19 +36,17 @@ function checkAll() {
   $("input:checkbox").prop('checked', true);
 }
 
-// adds the string info to the page
-function d(info) {
-  console.log(info);
-  $("#noticeboard").append("<br>" + info);
-}
-
-// ticks the checkboxes based on the values in the config file on the server
-function populateCheckboxValues(config) {
+// puts the information from the config file into the forms on the admin page
+function populateValues(config) {
   for (var key in config) {
-    // if the key matches the id of any element on this page, check its box
     $("#" + key).each(
       function() {
-        $(this).prop('checked', config[key]); 
+        if ($(this).is(':checkbox')) {
+          // if its a checkbox, we want to check it, not set its value
+          $(this).prop('checked', config[key]); 
+        } else {
+          $(this).val(config[key]);
+        }
       }
     )
   }
@@ -88,7 +55,7 @@ function populateCheckboxValues(config) {
 function getCheckboxValuesFromServer() {
   $.get("readConfigFile.php", 
     function(config) {
-      populateCheckboxValues(config);
+      populateValues(config);
     },
     'json'
   );
